fd8948dff86aaaee514b5f4308d04be98633b00b
phucnguyen <phucnguyendigital2003@gmail.com>
2025-08-07 17:52:34 +0700

    set up BOM step-1

---
+++ phuc-bom/src/test/java/com/phucnguyen/phucbom/PhucBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucnguyen.phucbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhucBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
